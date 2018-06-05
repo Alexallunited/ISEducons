944604b89ba8dc22bfeed3fe5e5d08d9f88caf21
Alexallunited <alexallunited@hotmail.com>
2018-06-05 16:42:27 +0200

    Očišćen kod u NetworkScanner-u

--- ISEducons/NetworkScanner.xaml.cs
+++ ISEducons/NetworkScanner.xaml.cs
@@ -56,17 +56,14 @@ namespace ISEducons
             Application.Current.Dispatcher.BeginInvoke(
             DispatcherPriority.Background,
             new Action(() => this.progressBar.Maximum = 254));
-            //progressBar.Maximum = 254;
 
             Application.Current.Dispatcher.BeginInvoke(
             DispatcherPriority.Background,
             new Action(() => this.progressBar.Value = 0));
-            //progressBar.Value = 0;
 
             Application.Current.Dispatcher.BeginInvoke(
             DispatcherPriority.Background,
             new Action(() => this.listView.Items.Clear()));
-            //listView.Items.Clear();
 
             for (int i = 1; i < 255; i++)
             {
@@ -77,12 +74,10 @@ namespace ISEducons
                 Application.Current.Dispatcher.BeginInvoke(
                 DispatcherPriority.Background,
                 new Action(() => this.labelaStatus.Foreground = new SolidColorBrush(Colors.Green)));
-                //labelaStatus.Foreground = new SolidColorBrush(Colors.Green);
 
                 Application.Current.Dispatcher.BeginInvoke(
                 DispatcherPriority.Background,
                 new Action(() => this.labelaStatus.Content = "Skeniram " + subnet + subnetn));
-                //labelaStatus.Content = "Skeniram: " +subnet + subnetn;
 
                 if (reply.Status == IPStatus.Success)
                 {
@@ -91,18 +86,11 @@ namespace ISEducons
 
                         addr = IPAddress.Parse(subnet + subnetn);
                         host = Dns.GetHostEntry(addr);
-                        //host = Dns.GetHostEntry(addr);
-
-
-
+                        
                         Application.Current.Dispatcher.BeginInvoke(
                          DispatcherPriority.Background,
                          new Action(() => this.listView.Items.Add(new ScanData { Ip = subnet + subnetn, Hostname = host.HostName, Status = "Aktivan" })));
-                         //this.listView.Items.Add(new ScanData { Ip = subnet + subnetn, Hostmame = host.HostName, Status = "Aktivan" });
-
-                        //listView.Items.Add(new ListViewItem(new String[] { subnet + subnetn, host.HostName, "Up" }));
-                        //listView.Items.Add(new System.Windows.Forms.ListViewItem(new String[] { subnet + subnetn, host.HostName, "Up" }));
-                        //listView.Items.Add(new ListViewItem());
+                         
                     }
                     catch
                     {
@@ -114,28 +102,28 @@ namespace ISEducons
                 Application.Current.Dispatcher.BeginInvoke(
                 DispatcherPriority.Background,
                 new Action(() => this.progressBar.Value += 1));
-                //progressBar.Value += 1;
+               
             }
 
             Application.Current.Dispatcher.BeginInvoke(
             DispatcherPriority.Background,
             new Action(() => this.start.IsEnabled = true));
-            //start.IsEnabled = true;
+            
 
             Application.Current.Dispatcher.BeginInvoke(
             DispatcherPriority.Background,
             new Action(() => this.stop.IsEnabled = false));
-            //stop.IsEnabled = false;
+            
 
             Application.Current.Dispatcher.BeginInvoke(
             DispatcherPriority.Background,
             new Action(() => this.boxIP.IsEnabled = true));
-            //boxIP.IsEnabled = true;
+            
 
             Application.Current.Dispatcher.BeginInvoke(
             DispatcherPriority.Background,
             new Action(() => this.labelaStatus.Content = "Skeniranje završeno"));
-            //labelaStatus.Content = "Završeno skeniranje";
+            
 
 
             int count = listView.Items.Count;
@@ -146,30 +134,16 @@ namespace ISEducons
 
         private void cmdScan_Click(object sender, EventArgs e)
         {
-            //this.Dispatcher.Invoke(() =>
-            // {
+            
             if (boxIP.Text == string.Empty)
             {
                 MessageBox.Show("No IP address entered.", "Error");
             }
             else
             {
-                string a = "192.168.1";
-
-                myThread = new Thread(() => Scan(a));
-
-                //Application.Current.Dispatcher.BeginInvoke(
-                //DispatcherPriority.Background,
-                //new Action(() => this.myThread = new Thread(() => Scan(boxIP.Text))));
                
-                //myThread = new Thread(() => Scan(boxIP.Text));
-
-                //Application.Current.Dispatcher.Invoke(DispatcherPriority.Normal, new Action(() =>
-                //{
-                //    myThread = new Thread(() => 
-                //    Scan(boxIP.Text));
-                //} 
-                //));
+                string a = boxIP.Text;
+                myThread = new Thread(() => Scan(a));
                 myThread.IsBackground = true;
 
                 myThread.Start();  //Invoke
@@ -182,16 +156,17 @@ namespace ISEducons
                 }
                 
             }
-            // });
-
-
-
 
         }
 
         private void cmdStop_Click(object sender, EventArgs e)
         {
             myThread.Suspend();
+
+            Application.Current.Dispatcher.BeginInvoke(
+            DispatcherPriority.Background,
+            new Action(() => this.progressBar.Value = 0));
+           
             start.IsEnabled = true;
             stop.IsEnabled = false;
             boxIP.IsEnabled = true;
